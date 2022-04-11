afa2e046c9e82c03c7fa4de193a03e8a497da86e
Pavlo <mongolzzz21@gmail.com>
2022-04-11 14:35:51 +0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld :vis="true"/>
+
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <style>
